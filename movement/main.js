de5525e6f57f462684844b7fdb088588ef71ea47
Deepak Yadav <deepakyadav11767@gmail.com>
2024-10-23 23:16:21 +0530

    Movement

---
+++ movement/main.js
@@ -0,0 +1,43 @@
+import * as THREE from "three";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 20);
+
+const geometry = new THREE.BoxGeometry(1,2,3);
+const material = new THREE.MeshBasicMaterial({color: "red"});
+const cube= new THREE.Mesh(geometry,material);
+
+camera.position.z = 5;
+
+
+scene.add(cube);
+
+
+
+const canvas = document.querySelector("canvas");
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.render(scene, camera);
+
+const controls = new OrbitControls( camera, renderer.domElement );
+const mouse = {
+  x:0,
+  y:0,
+};
+
+window.addEventListener("mousemove", function (e) {
+  mouse.x = e.clientX / window.innerWidth;
+  mouse.y = e.clientY / window.innerHeight;
+  console.log(e.clientX,e.clientY);
+});
+
+function animate(){
+    window.requestAnimationFrame(animate);
+    // cube.rotation.y += 0.01;
+    cube.lookAt(new THREE.Vector3(mouse.x-.5,-mouse.y+.5,1));
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+animate();
