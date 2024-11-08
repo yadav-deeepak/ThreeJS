a1ecebebb20e04d83c25b3ea2fc43801636974e2
Deepak Yadav <deepakyadav11767@gmail.com>
2024-11-08 22:55:32 +0530

    The Planet

--- ThePlanet/main.js
+++ ThePlanet/main.js
@@ -1,26 +1,47 @@
 import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 20);
 
-const geometry = new THREE.BoxGeometry(1,1,1,6,6,6);
-const material = new THREE.MeshBasicMaterial({color: "red", wireframe: true});
-const cube= new THREE.Mesh(geometry,material);
+
 
 camera.position.z = 5;
 
 
-scene.add(cube);
+
 
 const canvas = document.querySelector("canvas");
 const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.render(scene, camera);
 
+window.addEventListener("resize", function (e) {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    renderer.setSize( window.innerWidth, window.innerHeight);
+    camera.updateProjectionMatrix();
+  });
+
+const radius = 1;
+const segments = 32;
+const colors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00];
+const spheres = new THREE.Group();
+  
+for(let i = 0; i<4; i++){
+const geometry = new THREE.SphereGeometry(radius, segments,segments);
+const material = new THREE.MeshBasicMaterial({color: colors[i]});
+const sphere = new THREE.Mesh(geometry, material);
+spheres.add(sphere);
+}
+scene.add(spheres);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
 function animate(){
     window.requestAnimationFrame(animate);
-    cube.rotation.y += 0.01;
+    
+    controls.update();
     renderer.render(scene, camera);
 }
 
