604c8bd843ef14f193ed5f71230d46f53e260daa
Deepak Yadav <deepakyadav11767@gmail.com>
2024-11-09 22:49:28 +0530

    The Planet

--- ThePlanet/main.js
+++ ThePlanet/main.js
@@ -1,19 +1,20 @@
 import * as THREE from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import gsap from 'gsap';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 20);
+const camera = new THREE.PerspectiveCamera(25,window.innerWidth / window.innerHeight, 0.1, 100);
 
 
 
-camera.position.z = 5;
+camera.position.z = 9;
 
 
 
 
 const canvas = document.querySelector("canvas");
-const renderer = new THREE.WebGLRenderer({canvas});
+const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.render(scene, camera);
 
@@ -23,8 +24,8 @@ window.addEventListener("resize", function (e) {
     camera.updateProjectionMatrix();
   });
 
-const radius = 1;
-const segments = 32;
+const radius = 1.3;
+const segments = 64;
 const colors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00];
 const spheres = new THREE.Group();
   
@@ -32,9 +33,25 @@ for(let i = 0; i<4; i++){
 const geometry = new THREE.SphereGeometry(radius, segments,segments);
 const material = new THREE.MeshBasicMaterial({color: colors[i]});
 const sphere = new THREE.Mesh(geometry, material);
+
+const angle = (i / 4) * (Math.PI *2);
+const orbitRadius = 4.5;
+sphere.position.x = orbitRadius * Math.cos(angle);
+sphere.position.z = orbitRadius * Math.sin(angle);
+
 spheres.add(sphere);
 }
+spheres.rotation.x = 0.1;
+spheres.position.y = -0.8;
 scene.add(spheres);
+
+setInterval(()=>{
+  gsap.to(spheres.rotation, {
+    y: `+=${Math.PI / 2}`,
+    duration: 2,
+    ease: "expo.easeInOut",
+  });
+}, 2500);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
